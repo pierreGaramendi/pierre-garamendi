42b6a6fd75b83796db0b8989ef609aebbd9b7bc7
Pierre Garamendi <pierre.garamendi@gmail.com>
2024-09-10 10:25:16 -0500

    folder name changed

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CustomeCarouselComponent } from '../components/custome-carousel/custome-carousel.component';
-import { StreamsByContextComponent } from '../components/streamsbycontext/streams-by-context.component';
-import { LazyLoadDirective } from '../directives/lazyload.directive';
+import { StreamsByContextComponent } from '../components/streams-by-context/streams-by-context.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +9,7 @@ import { LazyLoadDirective } from '../directives/lazyload.directive';
   imports: [
     StreamsByContextComponent,
     CustomeCarouselComponent,
-    LazyLoadDirective,
+    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
